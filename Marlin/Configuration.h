94ecd1b31dee1ebd02f7541651bf82e57a52644a
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2021-05-11 18:13:16 -0400

    Lower mintemp to prevent repeated popup when printing at low temps.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -621,7 +621,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 205
+#define EXTRUDE_MINTEMP 170
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
