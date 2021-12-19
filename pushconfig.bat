8636a92245f8b8562df4c8e016a3398f6e59a8e7
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2021-12-19 17:48:29 -0500

    Detect Marlin in an alternate git directory.

--- pushconfig.bat
+++ pushconfig.bat
@@ -1,12 +1,16 @@
-IF NOT EXIST ..\Marlin\Marlin\Configuration.h GOTO ENDBAD
-copy Marlin-r2x_14t\Marlin\Configuration.h ..\Marlin\Marlin\
-IF NOT EXIST ..\Marlin\Marlin\Configuration_adv.h GOTO ENDBAD
-copy Marlin-r2x_14t\Marlin\Configuration_adv.h ..\Marlin\Marlin\
-IF NOT EXIST ..\Marlin\platformio.ini GOTO ENDBAD
-copy Marlin-r2x_14t\platformio.ini ..\Marlin\
+@echo off
+SET SRC_MARLIN=..\Marlin
+IF EXIST ..\..\git\Marlin\Marlin\Configuration.h SET SRC_MARLIN=..\..\git\Marlin
+ECHO INFO: %SRC_MARLIN% will be used since Marlin\Configuration.h was detected there.
+IF NOT EXIST %SRC_MARLIN%\Marlin\Configuration.h GOTO ENDBAD
+copy Marlin-r2x_14t\Marlin\Configuration.h %SRC_MARLIN%\Marlin\
+IF NOT EXIST %SRC_MARLIN%\Marlin\Configuration_adv.h GOTO ENDBAD
+copy Marlin-r2x_14t\Marlin\Configuration_adv.h %SRC_MARLIN%\Marlin\
+IF NOT EXIST %SRC_MARLIN%\platformio.ini GOTO ENDBAD
+copy Marlin-r2x_14t\platformio.ini %SRC_MARLIN%\
 GOTO ENDSILENT
 
 :ENDBAD
-echo The directory ..\Marlin doesn't seem to be the Marlin repo containing the Marlin subdirectory of the same name. First clone according to readme.md.
+echo The directory %SRC_MARLIN% doesn't seem to be the Marlin repo containing the Marlin subdirectory of the same name. First clone according to readme.md.
 
 :ENDSILENT
