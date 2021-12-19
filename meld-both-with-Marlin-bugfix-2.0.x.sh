6f6220786c9897352bf6453c57ecf2c1cdef3f39
poikilos <7557867+poikilos@users.noreply.github.com>
2021-12-19 17:10:14 -0500

    Set base to bugfix-2.0.x ecaebe4363 (config version 02000903). Use FlexionHT PID tuning values (Resolves #8).

---
+++ meld-both-with-Marlin-bugfix-2.0.x.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+meld /home/owner/Downloads/git/MarlinFirmware/Marlin ~/git/r2x_14t/Marlin-base ~/git/r2x_14t/Marlin-r2x_14t
+
---
+++ meld-with-Marlin-bugfix-2.0.x.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+meld /home/owner/Downloads/git/MarlinFirmware/Marlin ~/git/r2x_14t/Marlin-base
