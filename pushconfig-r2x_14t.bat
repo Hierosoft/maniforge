c65756296aac8689203e8cf6881975a8c26cfbff
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2021-12-22 15:05:34 -0500

    Rename a variable for clarity.

--- pushconfig-r2x_14t.bat
+++ pushconfig-r2x_14t.bat
@@ -1,16 +1,16 @@
 @echo off
-SET SRC_MARLIN=..\Marlin
-IF EXIST ..\..\git\Marlin\Marlin\Configuration.h SET SRC_MARLIN=..\..\git\Marlin
-ECHO INFO: %SRC_MARLIN% will be used since Marlin\Configuration.h was detected there.
-IF NOT EXIST %SRC_MARLIN%\Marlin\Configuration.h GOTO ENDBAD
-copy Marlin-r2x_14t\Marlin\Configuration.h %SRC_MARLIN%\Marlin\
-IF NOT EXIST %SRC_MARLIN%\Marlin\Configuration_adv.h GOTO ENDBAD
-copy Marlin-r2x_14t\Marlin\Configuration_adv.h %SRC_MARLIN%\Marlin\
-IF NOT EXIST %SRC_MARLIN%\platformio.ini GOTO ENDBAD
-copy Marlin-r2x_14t\platformio.ini %SRC_MARLIN%\
+SET DST_BRANCH=..\Marlin
+IF EXIST ..\..\git\Marlin\Marlin\Configuration.h SET DST_BRANCH=..\..\git\Marlin
+ECHO INFO: %DST_BRANCH% will be used since Marlin\Configuration.h was detected there.
+IF NOT EXIST %DST_BRANCH%\Marlin\Configuration.h GOTO ENDBAD
+copy Marlin-r2x_14t\Marlin\Configuration.h %DST_BRANCH%\Marlin\
+IF NOT EXIST %DST_BRANCH%\Marlin\Configuration_adv.h GOTO ENDBAD
+copy Marlin-r2x_14t\Marlin\Configuration_adv.h %DST_BRANCH%\Marlin\
+IF NOT EXIST %DST_BRANCH%\platformio.ini GOTO ENDBAD
+copy Marlin-r2x_14t\platformio.ini %DST_BRANCH%\
 GOTO ENDSILENT
 
 :ENDBAD
-echo The directory %SRC_MARLIN% doesn't seem to be the Marlin repo containing the Marlin subdirectory of the same name. First clone according to readme.md.
+echo The directory %DST_BRANCH% doesn't seem to be the Marlin repo containing the Marlin subdirectory of the same name. First clone according to readme.md.
 
 :ENDSILENT
