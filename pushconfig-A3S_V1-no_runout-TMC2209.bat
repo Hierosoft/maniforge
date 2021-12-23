170a3ac4eaff730a2729bf37f016bdb2bc8a52e4
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2021-12-23 02:20:41 -0500

    Change the batch to reflect the new directory name.

--- pushconfig-A3S_V1-no_runout-TMC2209.bat
+++ pushconfig-A3S_V1-no_runout-TMC2209.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET SRC_BRANCH=.\Marlin-A3S_V1-no_runout-TFT24
+SET SRC_BRANCH=.\Marlin-A3S_V1-no_runout-TMC2209
 SET DST_BRANCH=..\Marlin
 REM SET SRC_BRANCH=%USERPROFILE%\git\Configurations\config\examples\JGAurora\A3S_V1
 REM IF NOT EXIST "%SRC_BRANCH%" ECHO Error: There is no "%SRC_BRANCH%". If the Poikilos A3S_V1 configuration isn't pulled upstream yet, ensure you cloned the Poikilos' fork of the MarlinFirmware Configurations repo and that you have, in that directory, run "git switch bugfix-2.0.x-A3S_MK1".
