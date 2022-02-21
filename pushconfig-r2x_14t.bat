1cad36a01be8857353c2e562cfc62e4c6f48aec0
Jake Gustafson <7557867+poikilos@users.noreply.github.com>
2022-02-21 12:13:16 -0500

    Share the SRC_BRANCH_MARLIN batch variable. Rename ENDBAD to BAD_DST for clarity.

--- pushconfig-r2x_14t.bat
+++ pushconfig-r2x_14t.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
 SET SRC_BRANCH=.\Marlin-r2x_14t
+SET SRC_BRANCH_MARLIN=.\Marlin-r2x_14t\Marlin
 SET DST_BRANCH=..\Marlin
+REM IF EXIST ..\..\git\Marlin SET DST_BRANCH=..\..\git\Marlin
+REM ^ Done in pushconfig.bat
 CALL utilities/pushconfig.bat
--- utilities/pushconfig.bat
+++ utilities/pushconfig.bat
@@ -4,17 +4,23 @@ REM ^ Handle flat directory structures like the Configuration examples in Marlin
 IF EXIST ..\..\git\Marlin\Marlin\Configuration.h SET DST_BRANCH=..\..\git\Marlin
 ECHO INFO: %DST_BRANCH% will be used since Marlin\Configuration.h was detected there.
 IF NOT EXIST %DST_BRANCH%\Marlin\Configuration.h ECHO Error: There is no %DST_BRANCH%\Marlin\Configuration.h to replace.
-IF NOT EXIST %DST_BRANCH%\Marlin\Configuration.h GOTO ENDBAD
+IF NOT EXIST %DST_BRANCH%\Marlin\Configuration.h GOTO BAD_DST
 IF NOT EXIST %DST_BRANCH%\Marlin\Configuration_adv.h ECHO Error: There is no %DST_BRANCH%\Marlin\Configuration_adv.h replace.
-IF NOT EXIST %DST_BRANCH%\Marlin\Configuration_adv.h GOTO ENDBAD
+IF NOT EXIST %DST_BRANCH%\Marlin\Configuration_adv.h GOTO BAD_DST
 IF NOT EXIST %DST_BRANCH%\platformio.ini ECHO Error: There is no %DST_BRANCH%\platformio.ini to replace.
-IF NOT EXIST %DST_BRANCH%\platformio.ini GOTO ENDBAD
+IF NOT EXIST %DST_BRANCH%\platformio.ini GOTO BAD_DST
+IF NOT EXIST %SRC_BRANCH_MARLIN%\Configuration.h ECHO Error: There is no %SRC_BRANCH_MARLIN%\Configuration.h
+IF NOT EXIST %SRC_BRANCH_MARLIN%\Configuration.h GOTO BAD_SRC
+IF NOT EXIST %SRC_BRANCH_MARLIN%\Configuration_adv.h ECHO Error: There is no %SRC_BRANCH_MARLIN%\Configuration_adv.h
+IF NOT EXIST %SRC_BRANCH_MARLIN%\Configuration_adv.h GOTO BAD_SRC
 copy %SRC_BRANCH_MARLIN%\Configuration.h %DST_BRANCH%\Marlin\
 copy %SRC_BRANCH_MARLIN%\Configuration_adv.h %DST_BRANCH%\Marlin\
 copy %SRC_BRANCH%\platformio.ini %DST_BRANCH%\
 GOTO ENDSILENT
 
-:ENDBAD
+:BAD_DST
 echo The directory %DST_BRANCH% doesn't seem to be the Marlin repo containing the Marlin subdirectory of the same name. First clone according to readme.md.
+
+:BAD_SRC
 
 :ENDSILENT
